e1007b65595c5f46c61632f02633d535a44af0bd
dock <43358190+dock0d1@users.noreply.github.com>
2021-07-17 23:10:02 -0300

    Update cep.js

--- src/comandos/cep.js
+++ src/comandos/cep.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js');//Const
 const { color, prefix } = require('../../config.json');
 
 module.exports.run = (app, message, args) => {
